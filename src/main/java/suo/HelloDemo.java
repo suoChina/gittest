d206f53c9698c9e5ef6c58d1f45f1a7527125b9f
索亮 <95142427+suoChina@users.noreply.github.com>
2021-11-28 14:23:30 +0800

    Update HelloDemo.java

--- src/main/java/suo/HelloDemo.java
+++ src/main/java/suo/HelloDemo.java
@@ -23,5 +23,6 @@ public class HelloDemo {
         System.out.println("hello git8");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
