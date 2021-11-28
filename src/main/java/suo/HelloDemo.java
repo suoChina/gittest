122b04cbc4cde92b047411880172d724a13721a2
suo <suo@qq.com>
2021-11-28 13:31:04 +0800

    second commit

--- src/main/java/suo/HelloDemo.java
+++ src/main/java/suo/HelloDemo.java
@@ -13,5 +13,6 @@ public class HelloDemo {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
