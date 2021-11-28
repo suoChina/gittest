857147e1d1cc1516055f1653577f29c356f3dd2e
suo <suo@qq.com>
2021-11-28 13:54:28 +0800

    hot-fix commit2

--- src/main/java/suo/HelloDemo.java
+++ src/main/java/suo/HelloDemo.java
@@ -17,5 +17,7 @@ public class HelloDemo {
         System.out.println("hello git3");
         System.out.println("hello git4");
         System.out.println("hello git5");
+        System.out.println("hello git6");
+        System.out.println("hello git7");
     }
 }
