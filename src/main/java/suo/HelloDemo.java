da9309aad7eddfb1206004c0d921e279da7c7a89
suo <suo@qq.com>
2021-11-28 13:28:24 +0800

    first commit

---
+++ src/main/java/suo/HelloDemo.java
@@ -0,0 +1,17 @@
+package suo;
+
+/**
+ * describe:
+ *
+ * @author suoliang
+ * @create 2021-11-13:23
+ */
+public class HelloDemo {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
